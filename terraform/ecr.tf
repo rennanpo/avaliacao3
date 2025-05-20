d026e0ddb1fa227b6a74f6d9c4869db1988b2ffd
rennanpo <rennanpontescardoso@gmail.com>
2025-05-20 00:52:03 -0300

    tentando5

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "todo_app" {
-  name = "todo-app"
+  name = "todo-app-dp011"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
