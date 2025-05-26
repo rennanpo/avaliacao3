ac6aa940180767da4a42cc82ae2078a5d66f6dff
rennanpo <rennanpontescardoso@gmail.com>
2025-05-26 19:52:11 -0300

    imagem do docker hardcoded

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,8 +2,3 @@ variable "node_role_arn" {
   description = "ARN da IAM Role para os nós do EKS"
   type        = string
 }
-
-variable "todo_image" {
-  type        = string
-  description = "URI da imagem Docker"
-}
